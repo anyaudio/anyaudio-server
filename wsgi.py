d80b97e3552d796d9aaae77a3075bd1f50c692c4
aviaryan <avi.aryan123@gmail.com>
2016-08-04 15:40:21 +0530

    change port to 80

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ from app import app as application
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8080, application)
+    httpd = make_server('localhost', 80, application)
     # Wait for a single request, serve it and quit.
     httpd.handle_request()
