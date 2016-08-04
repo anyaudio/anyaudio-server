60adbf4ac4e3631a641bc8ae70b08839e089f225
aviaryan <avi.aryan123@gmail.com>
2016-08-04 16:24:47 +0530

    removed wsgi.py, was not needed

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-from app import app as application
-
-
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 80, application)
-    # Wait for a single request, serve it and quit.
-    httpd.handle_request()
