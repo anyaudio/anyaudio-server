7e1d40781cd92135281bbb5c422bf5fc95e0a7b5
aviaryan <avi.aryan123@gmail.com>
2016-08-04 15:05:21 +0530

    added files to help hosting on openshift

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from app import app as application
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    httpd.handle_request()
