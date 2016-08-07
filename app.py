424bb7c924710b7a1108f876a5358bc1f354a5d4
aviaryan <avi.aryan123@gmail.com>
2016-08-08 02:04:34 +0530

    reload with code change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from subprocess import call
 
 if __name__ == '__main__':
     # http://docs.gunicorn.org/en/stable/settings.html
-    cmd = 'gunicorn ymp3:app -w 4 --worker-class eventlet'
+    cmd = 'gunicorn ymp3:app -w 4 --worker-class eventlet --reload'
     cmd += ' -b %s:%s' % (
         environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1'),
         environ.get('OPENSHIFT_PYTHON_PORT', '5000')
