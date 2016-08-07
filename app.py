9424a92239212cf8e36048772344d0c0384084dc
aviaryan <avi.aryan123@gmail.com>
2016-08-08 00:27:28 +0530

    refactored code, removed wsgi code

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 from os import environ
 from subprocess import call
-from ymp3 import app, LOCAL
 
 
 if __name__ == '__main__':
+    # http://docs.gunicorn.org/en/stable/settings.html
     cmd = 'gunicorn ymp3:app -w 4 --worker-class eventlet'
     cmd += ' -b %s:%s' % (
         environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1'),
         environ.get('OPENSHIFT_PYTHON_PORT', '5000')
     )
     call(cmd.split())
-    # if LOCAL:
-    #     app.run(
-    #         host=environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1'),
-    #         port=int(environ.get('OPENSHIFT_PYTHON_PORT', 5000))
-    #     )
-    # else:
-    #     from wsgiref.simple_server import make_server
-    #     httpd = make_server(
-    #         environ.get('OPENSHIFT_PYTHON_IP'),
-    #         int(environ.get('OPENSHIFT_PYTHON_PORT')), app
-    #     )
-    #     httpd.serve_forever()
