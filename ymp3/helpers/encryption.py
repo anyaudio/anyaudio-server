ee191fdf58d5911beed27d47afab8c1e14a84b02
aviaryan <avi.aryan123@gmail.com>
2016-08-09 17:14:49 +0530

    added encryption in /d/ urls

---
+++ ymp3/helpers/encryption.py
@@ -0,0 +1,26 @@
+import base64
+import os
+# http://stackoverflow.com/a/16321853/2295672
+
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc))
+
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
+def get_key():
+    return os.environ.get('SECRET_KEY', 'default key')
--- ymp3/views.py
+++ ymp3/views.py
@@ -3,11 +3,11 @@ import traceback
 import logging
 from flask import jsonify, request, render_template, url_for, make_response
 from subprocess import check_output, call
-from base64 import b64encode, b64decode
 from ymp3 import app, LOCAL
 
 from helpers.search import get_videos, get_video_attrs
 from helpers.helpers import delete_file, get_ffmpeg_path
+from helpers.encryption import encode, decode, get_key
 
 
 @app.route('/')
@@ -22,7 +22,8 @@ def download_file(url):
     First downloads the file on the server using wget and then converts it using ffmpeg
     """
     try:
-        vid_id, url = b64decode(url).split(' ', 1)  # zz
+        # vid_id, url = b64decode(url).split(' ', 1)  # zz
+        vid_id, url = decode(get_key(), url).split(' ', 1)
         m4a_path = 'static/%s.m4a' % vid_id
         mp3_path = 'static/%s.mp3' % vid_id
         # vid_id regex is filename friendly [a-zA-Z0-9_-]{11}
@@ -63,7 +64,8 @@ def get_link(vid_id):
         retval = check_output(command.split())
         retval = retval.strip()
         if not LOCAL:
-            retval = b64encode(vid_id + ' ' + retval)
+            retval = encode(get_key(), vid_id + ' ' + retval)
+            # retval = b64encode(vid_id + ' ' + retval)
             retval = url_for('download_file', url=retval)
         return jsonify({'status': 0, 'url': retval})
     except Exception:
