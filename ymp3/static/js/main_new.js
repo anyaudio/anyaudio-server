cb82fb44d49fc17d3797a4d355376ecacfffcec7
ansarimofid <ansarimofid@gmail.com>
2016-08-21 04:05:58 +0530

    Trending songs can be listed on homepage

--- ymp3/static/js/main_new.js
+++ ymp3/static/js/main_new.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+	//Trending Playlist
+	var trendingList = ['popular','latest','weekly','electroic','latin'];
+
 	$('#ymp3-search').submit(function(e){
 		$('#search-preloader').show();
 		$('#result-keyword').hide();
@@ -39,12 +43,19 @@ $(document).ready(function(){
 	$('#nav-overlay .closebtn').click(function(e){
 		e.preventDefault();
 		document.getElementById("nav-overlay").style.width = 0;
-	})
+	});
 
+	trendingList.forEach(function(pName){
+		loadTrending(pName,4);
+	})
 });
 
 function getCardHtml(data){
 	return '<div class="col s6 m4 l3"><div class="card card--ym3"><a data-get-url="'+data['get_url']+'" class="card-image waves-effect waves-block waves-light ymp3-download"><img class="activator" src="'+data["thumb"]+'"><div class="overlay overlay--dark"></div><div class="meta-duration ">'+data["length"]+'</div><span href="#!" class="btn-dwn valign-wrapper"><i class="valign fa fa-spinner fa-pulse fa-3x fa-fw"></i><i class="valign fa fa-arrow-down" aria-hidden="true"></i><i class="valign fa fa-check fa-2x" aria-hidden="true"></i></span></a><div class="card-content"><span class="activator card--ym3--title flow-text">'+data["title"]+'</span><div class="meta"><div class="channel color-primary"><i class="fa fa-bullseye" aria-hidden="true"></i>'+data["uploader"]+'</div><div class="views color-primary"><i class="fa fa-eye" aria-hidden="true"></i>'+data["views"]+'</div></div></div><div class="card-reveal"><span class="card-title color-primary activator"><i class="fa fa-times right"></i>'+data["title"]+'</span><p class="flow-text">Here is some more information about this product that is only revealed once clicked on.</p></div></div></div>';
+}
+
+function getTrendingHtml(data,type){
+	return '<div class="trending"><div class="trending-title"><h4 class="title-deco title-deco--sm">'+type+'</h4></div><div class="white-space space-mini"></div><div class="trending-list row">'+data+'</div><div class="white-space space-mini"></div><a href="#!" class="no-shadow waves-effect waves-light btn red trending-more">More</a></div>';
 }
 
 function loadResult(searchInput){
@@ -53,6 +64,12 @@ function loadResult(searchInput){
 	$.getJSON('/api/v1/search?q=' + searchInput, success=function(data, textStatus, jqXHR){
 		var dataResult = data['results'];
 		var searchKeyword = data['metadata']['q'];
+		if(!dataResult.length){
+			$('#result-keyword h4').html('No <span class="color-primary">"result" </span>found');
+			$('#result-keyword').show();
+			$('#search-preloader').hide();
+			return false;
+		}
 		$('#result-keyword h4').html('Showing results for <span class="color-primary">"'+searchKeyword+'"</span>');
 		console.log(searchKeyword);
 		//console.log(dataResult);
@@ -66,29 +83,15 @@ function loadResult(searchInput){
 	})
 }
 
-//Adding Navbar dynamic class
+function loadTrending(type,number){
+	$.getJSON('/api/v1/trending?type=' + type+'&number='+number, success=function(data, textStatus, jqXHR){
+		var dataResult = data['results'];
+		var searchKeyword = data['metadata']['type'];
+		var trendingCard = '';
 
-(function($) {
-	"use strict";
-
-	var $navbar = $(".nav--custom"),
-		y_pos = $navbar.offset().top,
-		height = $navbar.height();
-
-	$(document).scroll(function() {
-		var scrollTop = $(this).scrollTop();
-
-		if (scrollTop > y_pos + height) {
-			$navbar.addClass("nav-white").animate({
-				top: 0
-			});
-			$navbar.removeClass("nav--transparent");
-		} else if (scrollTop <= y_pos) {
-			$navbar.removeClass("nav-white").clearQueue().animate({
-				top: "-80px"
-			}, 0);
-			$navbar.addClass("nav--transparent");
-		}
-	});
-
-})(jQuery, undefined);
+		dataResult.forEach(function(res){
+			trendingCard += getCardHtml(res);
+		});
+		$('#home-trending').append(getTrendingHtml(trendingCard,type));
+	})
+}
