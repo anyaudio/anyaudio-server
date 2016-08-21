51a19f4ab8be116007edd9388b726df16a1ce9f6
Avi Aryan <avi.aryan123@gmail.com>
2016-08-21 17:25:21 +0530

    Added test for trending API (#57)
    * added test for trending
    
    * use thread.join to wait for trending thread to complete
    
    * run in daemon mode
    
    * thread wait in same method
    
    * set run_repeater as run in tests
    
    * test all playlists
    
    * output failed playlist name

---
+++ tests/test_trending.py
@@ -0,0 +1,27 @@
+import unittest
+import json
+from tests import YMP3TestCase
+from ymp3.schedulers import trending
+from ymp3.helpers.data import trending_playlist
+
+
+class TestTrending(YMP3TestCase):
+    """
+    Test Trending
+    """
+    def test_trending_worker_run(self):
+        """run trending worker and see if it goes well"""
+        scheduler = trending.TrendingScheduler()
+        scheduler.run_repeater = scheduler.run
+        worker = scheduler.start()
+        worker.join()
+        # ^^ wait for above to finish
+        for playlist in trending_playlist:
+            resp = self.app.get('/api/v1/trending?type=%s' % playlist[0])
+            self.assertEqual(resp.status_code, 200, playlist[0])
+            data = json.loads(resp.data)
+            self.assertNotEqual(data['metadata']['count'], 0, playlist[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
