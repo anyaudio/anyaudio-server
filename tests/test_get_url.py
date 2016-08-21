ed9de1a7c50f49e4ee54f26c9c32d9ed696ec5e1
aviaryan <avi.aryan123@gmail.com>
2016-08-21 14:55:08 +0530

    fixed incorrect padding issue in get url phase

--- tests/test_get_url.py
+++ tests/test_get_url.py
@@ -20,6 +20,17 @@ class TestGetUrl(YMP3TestCase):
         resp = self.app.get('/api/v1/g?url=somefalseurl')
         self.assertEqual(resp.status_code, 500)
 
+    def test_get_url_b64_padding_issue(self):
+        """
+        test b64 decoding of url which used to throw IncorrectPadding error
+        PS - Didn't had another sample with the same behavior
+        """
+        result = self._search('Hot Desi Naukrani Ke Sath Romance', just_results=True)
+        get_url = result[0]['get_url']
+        resp = self.app.get(get_url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn('url', resp.data)
+
 
 if __name__ == '__main__':
     unittest.main()
