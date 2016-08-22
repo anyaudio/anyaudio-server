131f3f6b305802820e7bcf6ec8cd3436b06f6eea
aviaryan <avi.aryan123@gmail.com>
2016-08-22 20:05:05 +0530

    added a test for m4a download

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,5 +1,6 @@
 import unittest
 from tests import YMP3TestCase
+from ymp3 import DOWNLOAD_MP3
 
 
 class TestDownload(YMP3TestCase):
@@ -24,6 +25,25 @@ class TestDownload(YMP3TestCase):
         resp = self.app.get('/api/v1/d?url=askfasfk')
         self.assertEqual(resp.status_code, 500)
         self.assertTrue(len(resp.data) < 1000)
+
+    def test_successful_download_m4a(self):
+        """test successful download of a music in m4a"""
+        global DOWNLOAD_MP3
+        dmp3_copy = DOWNLOAD_MP3
+        DOWNLOAD_MP3 = False
+        # search and get link
+        result = self._search('Payphone Maroon 5', just_results=True)
+        get_url = result[0]['get_url']
+        dl_url = self._get_dl_link(get_url, just_url=True)
+        resp = self.app.get(dl_url)
+        # test
+        self.assertTrue(len(resp.data) > 100000, resp.data)
+        self.assertEqual(int(resp.headers['Content-Length']), len(resp.data))
+        self.assertIn(
+            '.m4a', resp.headers['Content-Disposition'], resp.headers['Content-Disposition']
+        )
+        # change to back
+        DOWNLOAD_MP3 = dmp3_copy
 
 
 if __name__ == '__main__':
