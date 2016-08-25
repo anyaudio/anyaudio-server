34a16f777e77f362264f509fec38be852bcd9cad
aviaryan <avi.aryan123@gmail.com>
2016-08-25 14:58:41 +0530

    added new homepage to /beta

--- ymp3/views.py
+++ ymp3/views.py
@@ -20,20 +20,27 @@ from helpers.networking import open_page
 @app.route('/')
 @record_request
 def home():
+    return render_template('/home.html')
+
+
+@app.route('/beta')
+@record_request
+def home_beta():
     return render_template('/index.html')
+
 
 @app.route('/explore')
 @record_request
 def explore():
     search_query = request.args.get('q')
     if search_query:
-        search_query = '"{0}"'.format(search_query.replace('\"','\\\"').strip())
+        search_query = '"{0}"'.format(search_query.replace('\"', '\\\"').strip())
     else:
         search_query = '""'
 
     playlist = request.args.get('p')
     if playlist:
-        playlist = '"{0}"'.format(playlist.replace('\"','\\\"').strip())
+        playlist = '"{0}"'.format(playlist.replace('\"', '\\\"').strip())
     else:
         playlist = '""'
     return render_template('/explore.html', query=Markup(search_query), playlist=Markup(playlist))
