3d24017e8a81e28e32ce9473652b6833a81be52b
Avi Aryan <avi.aryan123@gmail.com>
2017-01-17 17:18:12 +0530

    export miniconda PATH in git hook

--- scripts/ec2_hook.sh
+++ scripts/ec2_hook.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export PATH="/home/ubuntu/miniconda2/bin:$PATH"
+
 while read oldrev newrev ref
 do
   branch=`echo $ref | cut -d/ -f3`
@@ -7,7 +9,9 @@ do
     echo 'Changes pushed to Amazon EC2 PROD.'
     cd /home/ubuntu/app
     pip install --upgrade -r requirements.txt --no-cache-dir
+    echo 'Python requirements upgraded'
     pkill -f gunicorn || true
+    echo 'Killed old instance'
     echo 'Server is live'
   fi
 done
