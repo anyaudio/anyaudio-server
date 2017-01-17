cf6e6daa76e0eaf4a76e3ccd7806f39edc936f77
Avi Aryan <avi.aryan123@gmail.com>
2017-01-17 17:15:03 +0530

    improved post-push receive hook

---
+++ scripts/ec2_hook.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while read oldrev newrev ref
+do
+  branch=`echo $ref | cut -d/ -f3`
+  if [ "ec2" == "$branch" -o "master" == "$branch" ]; then
+    git --work-tree=/home/ubuntu/app/ checkout -f $branch
+    echo 'Changes pushed to Amazon EC2 PROD.'
+    cd /home/ubuntu/app
+    pip install --upgrade -r requirements.txt --no-cache-dir
+    pkill -f gunicorn || true
+    echo 'Server is live'
+  fi
+done
