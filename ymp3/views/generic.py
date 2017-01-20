2efbd75ff9f55d05492b7fc02c4c4f334efe3486
Pratyush Singh <singh.pratyush96@gmail.com>
2017-01-20 20:43:14 +0530

    Templatized lite section to provide large page count for website

--- ymp3/views/generic.py
+++ ymp3/views/generic.py
@@ -1,12 +1,56 @@
+from flask import redirect
 from flask import render_template, Markup, request
+from flask import url_for
+
 from ymp3 import app
-from ymp3.helpers.helpers import record_request
+from ymp3.helpers.encryption import encode_data, get_key
+from ymp3.helpers.helpers import record_request, get_download_link_youtube
+from ymp3.helpers import database
+from ymp3.helpers.search import get_search_results_html, get_videos, \
+    get_video_attrs, get_suggestions
 
 
-@app.route('/lite')
+@app.route('/lite', strict_slashes=False)
 @record_request
 def home():
-    return render_template('/home.html')
+    popular_searches = database.get_popular_searches(number=50)
+    return render_template('/home.html', searches=popular_searches)
+
+
+@app.route('/lite/search')
+@record_request
+def lite_search():
+    search_term = request.args.get('q', None)
+    if search_term is None:
+        return redirect('/lite')
+    raw_html = get_search_results_html(search_term)
+    vids = get_videos(raw_html)
+    ret_vids = []
+    for _ in vids:
+        temp = get_video_attrs(_, removeLongResult=True)
+        if temp:
+            temp['get_url'] = '/api/v1' + temp['get_url']
+            temp['stream_url'] = '/api/v1' + temp['stream_url']
+            temp['suggest_url'] = temp['get_url'].replace('/g?', '/suggest?',
+                                                          1)
+            ret_vids.append(temp)
+    return render_template('/lite_search.html', results=ret_vids,
+                           term=search_term)
+
+
+@app.route('/lite/music/<id>')
+@record_request
+def serve_music_lite(id):
+    video = {}
+    url = get_download_link_youtube(
+        id, 'webm[abr<=64]/webm[abr<=80]/m4a[abr<=64]/[abr<=96]/m4a')
+    stream_url = url_for('stream_handler', url=encode_data(get_key(), url=url))
+    url = get_download_link_youtube(id, 'm4a/bestaudio')
+    download_url = url_for('download_file', url=encode_data(get_key(), url=url))
+    video['stream_url'] = stream_url
+    video['download_url'] = download_url
+    video['suggestions'] = get_suggestions(id)
+    return render_template('/lite_song.html', video=video)
 
 
 @app.route('/beta')
